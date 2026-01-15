2433dd995c85178dbceac3258491bab9089e4790
Rodrigo Santamaria <jrodsc@iastate.edu>
2026-01-14 23:49:37 -0600

    Paper added

---
+++ OptimalHints/temp.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <fstream>
+#include <random>
+
+using namespace std;
+
+const int N = 100;
+const int OPT = N * 10;
+
+int f (int )
+
+int main(int argc, char * argv[]) {
+
+
+
+    return 0;
+}
